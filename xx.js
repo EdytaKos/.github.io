a839ebc1d6acdd1dd23fb9d1093b6ed9118d11ab
EdytaKos <77529250+EdytaKos@users.noreply.github.com>
2021-01-16 10:17:09 +0100

    Add files via upload

---
+++ xx.js
@@ -0,0 +1,7 @@
+const mobileNav= document.querySelector("mobile-nav");
+const mobileIcon= document.querySelector("mobile-icon");
+
+menuIcon.addEventListener("click", ()=>{
+    mobileNav .classList .toggle ("active")
+});
+
